34a974e52c3c8d65d5412a1eb841e95fb90cd790
James Etchells <etch.rush@gmail.com>
2022-05-27 12:53:05 -0700

    Fix security issues

--- app.js
+++ app.js
@@ -16,8 +16,7 @@ var app = express();
 //Set up mongoose connection
 require('dotenv').config();
 var mongoose = require('mongoose');
-var dev_db_url = 'mongodb+srv://Etchmon:local-library@cluster0.vmlggqh.mongodb.net/local_library?retryWrites=true&w=majority';
-var mongoDB = process.env.MONGODB_URI || dev_db_url;
+var mongoDB = process.env.MONGODB_URI;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
