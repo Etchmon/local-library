56e67c513c40e739cf8d1ce2263509105f3bacca
James Etchells <etch.rush@gmail.com>
2022-05-27 12:51:55 -0700

    Add .env and .gitignore

--- app.js
+++ app.js
@@ -14,8 +14,10 @@ var helmet = require('helmet');
 var app = express();
 
 //Set up mongoose connection
+require('dotenv').config();
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://Etchmon:local-library@cluster0.vmlggqh.mongodb.net/local_library?retryWrites=true&w=majority';
+var dev_db_url = 'mongodb+srv://Etchmon:local-library@cluster0.vmlggqh.mongodb.net/local_library?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
